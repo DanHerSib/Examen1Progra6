04e93eb3cbab8467d72c03bd39a71d7d829c2124
DanHerSib <Daniel@LAPTOP-TK7R86K2>
2021-10-22 22:47:33 -0600

    Methods added

---
+++ WebApp/Entity/TipoInquilinoEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entity
+{
+    public class TipoInquilinoEntity
+    {
+        public int? Id_TipoInquilino { get; set; }
+        public string Descripcion { get; set; }
+        public int? Estado { get; set; }
+    }
+}
