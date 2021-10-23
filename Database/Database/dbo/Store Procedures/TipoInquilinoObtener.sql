f56d4e2e9e4342c12715e02dd305d1daf01b993d
DanHerSib <Daniel@LAPTOP-TK7R86K2>
2021-10-23 01:16:52 -0600

    Modify Procedure

--- Database/Database/dbo/Store Procedures/TipoInquilinoObtener.sql
+++ Database/Database/dbo/Store Procedures/TipoInquilinoObtener.sql
@@ -7,5 +7,5 @@ AS BEGIN
         T.Id_TipoInquilino, T.Descripcion, T.Estado
         FROM dbo.TipoInquilino T
     WHERE
-    (Id_TipoInquilino IS NULL OR Id_TipoInquilino = @Id_TipoInquilino)
+    (@Id_TipoInquilino IS NULL OR Id_TipoInquilino = @Id_TipoInquilino)
 END 
